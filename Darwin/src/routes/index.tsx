99680f3682d12fef30c979b67ab830f103908602
Ezilryb <ezilrybtrash@gmail.com>
2026-08-28 22:13:33 +0200

    Delete Darwin directory

--- Darwin/src/routes/index.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import { LabApp } from "@/components/lab-app";
-
-export const Route = createFileRoute("/")({ component: Home });
-
-function Home() {
-  return <LabApp />;
-}
